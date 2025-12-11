6f24db840e3576219e305a4e500bf8074f89bade
Kevin Sclafani <145733929+quiet-ghost@users.noreply.github.com>
2025-12-11 18:02:50 -0500

    feat: Day 3

--- 2025/Day3/src/main/java/dev/quietghost/part2.java
+++ 2025/Day3/src/main/java/dev/quietghost/part2.java
@@ -5,6 +5,36 @@ import java.io.FileReader;
 
 public class part2 {
   public static void main(String[] args) throws Exception {
-    BufferedReader file = new BufferedReader(new FileReader("src/main/java/dev/quietghost/files/sample.txt"));
+    String line;
+    long sum = 0;
+
+    BufferedReader file = new BufferedReader(new FileReader("src/main/java/dev/quietghost/files/input.txt"));
+    while ((line = file.readLine()) != null) {
+      int[] digits = line.chars().map(Character::getNumericValue).toArray();
+      System.out.println("Digit Bank -> " + java.util.Arrays.toString(digits));
+
+      StringBuilder result = new StringBuilder();
+      int currentStart = 0;
+      int digitsRemaining = 12;
+      for (int position = 0; position < 12; position++) {
+        int searchEnd = digits.length - digitsRemaining;
+
+        int maxDigit = -1;
+        int maxIndex = -1;
+        for (int i = currentStart; i <= searchEnd; i++) {
+          if (digits[i] > maxDigit) {
+            maxDigit = digits[i];
+            maxIndex = i;
+          }
+        }
+        result.append(maxDigit);
+        currentStart = maxIndex + 1;
+        digitsRemaining--;
+      }
+      sum += Long.parseLong(result.toString());
+      System.out.println("Joltage -> " + result);
+    }
+    System.out.println("Sum -> " + sum);
+    file.close();
   }
 }
---
+++ 2025/Day4/part1.java
@@ -0,0 +1,4 @@
+public class part1 {
+  public static void main(String[] args) {
+  }
+}
